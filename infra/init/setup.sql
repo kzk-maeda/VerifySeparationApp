b5d8c54575c78c007a17e04a10e4541061698935
kzk-maeda <casino.music0711@gmail.com>
2019-12-21 02:18:07 +0900

    create docker definition

---
+++ infra/init/setup.sql
@@ -0,0 +1,33 @@
+create database gochat;
+\connect gochat;
+
+drop table if exists user_table;
+drop table if exists todo_table;
+drop table if exists session_table;
+
+create table user_table (
+    id         serial primary key,
+    uuid       varchar(64) not null unique,
+    name       varchar(255),
+    email      varchar(255) not null unique,
+    password   varchar(255) not null,
+    created_at timestamp not null   
+);
+
+create table todo_table (
+    id         serial primary key,
+    uuid       varchar(64) not null unique,
+    title      varchar(255),
+    body       text,
+    deadline   date,
+    user_id    integer references users(id),
+    created_at timestamp not null  
+);
+
+create table session_table (
+    id         serial primary key,
+    uuid       varchar(64) not null unique,
+    email      varchar(255),
+    user_id    integer references users(id),
+    created_at timestamp not null   
+)
