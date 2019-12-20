3810c95810f72f96e14fe6a9ba052d572d94a906
kzk-maeda <casino.music0711@gmail.com>
2019-12-21 02:27:31 +0900

    native project is created

---
+++ native/VerifySeparationApp/Pods/Target Support Files/Pods-VerifySeparationApp/Pods-VerifySeparationApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VerifySeparationApp : NSObject
+@end
+@implementation PodsDummy_Pods_VerifySeparationApp
+@end
---
+++ native/VerifySeparationApp/VerifySeparationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerifySeparationApp
+//
+//  Created by Kazuki Maeda on 2019/12/21.
+//  Copyright © 2019 Kazuki Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
